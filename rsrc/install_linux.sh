502523aa60c483dc245358918f40e1da2d49679e
D. Shwagginz <devin@shwaggi.nz>
2025-02-11 16:30:33 -0700

    test

--- rsrc/install_linux.sh
+++ rsrc/install_linux.sh
@@ -1,5 +1,5 @@
 sudo cp ./rsrc/linux/sunvox.so /usr/lib/libsunvox.so
 sudo ln -s /usr/lib/libsunvox.so /lib/sunvox.so
-sudo cp ./rsrc/linux/raylib.so /usr/local/lib/libraylib.so.4.5.0
-sudo cp /usr/local/lib/libraylib.so.4.5.0 /usr/lib/libraylib.so.450
-sudo ln -s /usr/lib/libraylib.so.450 /lib/raylib.so
+sudo cp ./rsrc/linux/raylib.so /usr/local/lib/libraylib.so.5.0.0
+sudo cp /usr/local/lib/libraylib.so.5.0.0 /usr/lib/libraylib.so.500
+sudo ln -s /usr/lib/libraylib.so.500 /lib/raylib.so
