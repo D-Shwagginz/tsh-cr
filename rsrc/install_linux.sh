d475ab1b45ad3fb7dd9e1dbbb8fc8b8953c9430f
D. Shwagginz <devin@shwaggi.nz>
2025-02-11 16:27:09 -0700

    test

--- rsrc/install_linux.sh
+++ rsrc/install_linux.sh
@@ -1,4 +1,4 @@
-cp ./rsrc/linux/sunvox.so /usr/lib/libsunvox.so
-ln -s /usr/lib/libsunvox.so /lib/sunvox.so
-cp ./rsrc/linux/raylib.so /usr/lib/libraylib.so.450
-ln -s /usr/lib/libraylib.so.450 /lib/raylib.so
+sudo cp ./rsrc/linux/sunvox.so /usr/lib/libsunvox.so
+sudo ln -s /usr/lib/libsunvox.so /lib/sunvox.so
+sudo cp ./rsrc/linux/raylib.so /usr/lib/libraylib.so.450
+sudo ln -s /usr/lib/libraylib.so.450 /lib/raylib.so
